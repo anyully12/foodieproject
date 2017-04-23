8a9af8a5cd84b92932c6663d13dba156d6ef8e42
Anyully Rosales <anyully12@gmail.com>
2017-04-22 20:47:46 -0400

    scaffold restaurant add seed implement loop view for restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(
+		[
+			{name: 'Zak The Baker', website: 'https://zakthebaker.com/', picture: 'https://zakthebaker.files.wordpress.com/2014/05/cropped-monogram-logo2-e1400508464403.jpg'},
+			{name: 'Love Life Wellness Cafe', website: 'http://lovelifewellnesscenter.com/', picture: 'http://images2.miaminewtimes.com/imager/choices-cafe/u/580x328/6422837/9442976.0.jpg'},
+			{name: 'KYU', website: 'http://kyumiami.com/', picture: 'http://kyumiami.com/wp-content/uploads/2016/02/KYU_6257.jpg'},
+			{name: 'Alter', website: 'http://www.altermiami.com/', picture: 'http://www.miami.com/wp-content/uploads/2015/07/alt1_0.jpg'},
+			{name: 'Della Test Kitchen', website: 'http://www.dellabowls.com/menu', picture: 'https://media.timeout.com/images/103479185/630/472/image.jpg'}
+		])
